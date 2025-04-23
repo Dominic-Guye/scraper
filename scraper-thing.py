2b82ed43fea35ea1e67ee275f6a806eb4d8b3faa
Dominic--Guye <sr8042mu@go.minnstate.edu>
2025-04-23 01:28:56 -0500

    Large-scale changes. I'm actually scraping now!

--- scraper-thing.py
+++ scraper-thing.py
@@ -1,21 +1,20 @@
-import urllib3
 import requests
 import bs4
+import html5lib
 
 
-example_response = requests.request('get','http://www.example.com')
-read_response = example_response.read()
-print(read_response)
-
-ortlund = urllib3.request('get','https://truthunites.org')
-ortlund_headers = ortlund.headers
-ortlund_info = ortlund.info()
-ortlund_status = ortlund.status
-ortlund_reason = ortlund.reason
-ortlund_read = ortlund.data
-if len(ortlund_read) > 10:
-    print(ortlund_read)
-elif len(ortlund_read) in (None,0):
+examplepage = requests.get('https://example.org')
+example_headers = examplepage.headers
+example_status = examplepage.status_code
+example_read = examplepage.text
+if len(example_read) > 10:
+    print(example_read)
+elif len(example_read) in (None, 0):
     raise ValueError("The response was null or zero.")
-elif ortlund_read != None:
-    raise urllib3.exceptions.ResponseError(f"There was a response, but it was way too short. It consisted entirely of {print(ortlund_read)}.")
+elif example_read != None:
+    raise urllib3.exceptions.ResponseError(f"There was a response, but it was way too short. It consisted entirely of {print(example_read)}.")
+
+example_soup = bs4.BeautifulSoup(example_read,'html5lib')
+print(example_soup)
+print(example_soup.find('a'))
+print(example_soup.find_all('p')) #Interesting, 'find_all' returns what seems like a list object
