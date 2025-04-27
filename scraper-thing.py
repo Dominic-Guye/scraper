2ce5200999ce64e05f0f51dbf44a39811877976c
Dominic--Guye <sr8042mu@go.minnstate.edu>
2025-04-26 22:23:30 -0500

    Added many annotations

--- scraper-thing.py
+++ scraper-thing.py
@@ -3,10 +3,12 @@ import bs4
 import html5lib
 
 
-examplepage = requests.get('https://example.org')
-example_headers = examplepage.headers
-example_status = examplepage.status_code
-example_read = examplepage.text
+examplepage = requests.get('https://example.org') # the actual request
+example_headers = examplepage.headers # storing the headers in a variable
+example_status = examplepage.status_code # storing the status code
+example_read = examplepage.text # storing the actual content of the page
+
+# Code to deny responses that are too short
 if len(example_read) > 10:
     print(example_read)
 elif len(example_read) in (None, 0):
@@ -14,7 +16,8 @@ elif len(example_read) in (None, 0):
 elif example_read != None:
     raise urllib3.exceptions.ResponseError(f"There was a response, but it was way too short. It consisted entirely of {print(example_read)}.")
 
+# The actual scraping occurs below
 example_soup = bs4.BeautifulSoup(example_read,'html5lib')
 print(example_soup)
-print(example_soup.find('a'))
+print(example_soup.find('a')) # find hyperlinks
 print(example_soup.find_all('p')) #Interesting, 'find_all' returns what seems like a list object
