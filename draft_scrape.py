b4a63325a40666f746f965c8d71767d072e3913b
Dominic--Guye <sr8042mu@go.minnstate.edu>
2025-04-29 02:13:43 -0500

    This is a MASSIVE update. Added more functions, error handling, conditionals, and more. Also moved some test code to a draft page.

---
+++ draft_scrape.py
@@ -0,0 +1,28 @@
+import requests
+import bs4
+import html5lib
+from time import sleep
+examplepage = requests.get('https://quotes.toscrape.com') # the actual request
+example_headers = examplepage.headers # storing the headers in a variable
+example_status = examplepage.status_code # storing the status code
+example_read = examplepage.text # storing the actual content of the page
+
+# Code to deny responses that are null or too short
+if len(example_read) > 10:
+    print("The page looks good so far! INCOMING!")
+    sleep(3)
+elif len(example_read) in (None, 0):
+    raise ValueError("The response was null or zero.")
+elif example_read != None:
+    raise urllib3.exceptions.ResponseError(f"There was a response, but it was way too short. It consisted entirely of {print(example_read)}.")
+
+# The actual scraping occurs below
+example_soup = bs4.BeautifulSoup(example_read,'html5lib')
+print('-------------START OF PAGE-------------\n')
+print(example_soup)
+print('\n-------------END OF PAGE-------------')
+print(example_soup.find('a')) # find hyperlinks
+found_elements = example_soup.find_all('p') #Interesting, 'find_all' returns what seems like a list object
+if isinstance(found_elements,list):
+    for i in found_elements:
+        print(i)
--- scraper-thing.py
+++ scraper-thing.py
@@ -2,42 +2,68 @@ import requests
 import bs4
 import html5lib
 
-examplepage = requests.get('https://example.org') # the actual request
-example_headers = examplepage.headers # storing the headers in a variable
-example_status = examplepage.status_code # storing the status code
-example_read = examplepage.text # storing the actual content of the page
-
-# Code to deny responses that are null or too short
-if len(example_read) > 10:
-    print(example_read)
-elif len(example_read) in (None, 0):
-    raise ValueError("The response was null or zero.")
-elif example_read != None:
-    raise urllib3.exceptions.ResponseError(f"There was a response, but it was way too short. It consisted entirely of {print(example_read)}.")
-
-# The actual scraping occurs below
-example_soup = bs4.BeautifulSoup(example_read,'html5lib')
-print(example_soup)
-print(example_soup.find('a')) # find hyperlinks
-print(example_soup.find_all('p')) #Interesting, 'find_all' returns what seems like a list object
-
 #maybe I should make a function for requesting (+ maybe storing) and another for scraping
+## Update: made a function for requesting, but storing will be hard bc of scope issues
 
 # the requesting function
-def get_page(url):
+def get_page(url: str = None) -> requests.models.Response: #note: first ever type hint!
     #if no url is given, use example.org
     if url == None:
-        url = "http://example.org"
+        inputted_url = input("What page do you want to request? Include the URL schema in your input. ")
+        if inputted_url != None:
+            url = inputted_url
+        else:
+            url = "http://example.org"
     #do the actual request
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.MissingSchema:
+        print("We couldn't recognize a URL schema. I TOLD you to include an URL schema! SMH.\nWe're just going to use 'https://'.")
+        response = requests.get("https://" + url)
     #do not continue and just raise an exception if there is an HTTP error code
     response.raise_for_status()
-
-    pass #I want more logic here, maybe for storing stuff or doing non-scraping analysis. IDK yet.
 
     #return the response object as the function's output
     return response
 
-def scrape():
-    # Need to do the scraping stuff here. Maybe later
-    pass
+# response1 = response # saving function output in the general scope # ugh, screwed this up, commenting out for now
+
+def read_response(resp: requests.models.Response) -> str:
+    '''Given a requests Response object, return the HTML source code'''
+    text = resp.text
+    # Code to deny responses that are null or too short
+    if len(text) > 10:
+        return text
+    elif len(text) in (None, 0):
+        raise ValueError("The response was null or zero.")
+    elif text != None:
+        raise requests.exceptions.RequestException(f"There was a response, but it was way too short. It consisted entirely of {print(example_read)}.")
+
+def scrape(html: str):
+    '''Scrape an HTML document. HTML should be in the form of a string'''
+    soup = bs4.BeautifulSoup(html,'html5lib')
+    while element_name != "NONE":
+        element_name = input("Which HTML element are you looking for? ")
+        found_elements = soup.find_all(element_name)
+        if isinstance(found_elements, list):
+            for e in found_elements:
+                print(e)
+
+#the main function
+if __name__ == '__main__':
+    #Note: I need to add exception handling
+    try:
+        page = get_page()
+    except requests.exceptions.HTTPError as http:
+        print(f"Ugh. There was an HTTP status code! Specifically, it was {http}.")
+    except requests.exceptions.RetryError as gaveUp:
+        print("We gave up. Too many tries, and they all failed.")
+        print(gaveUp)
+    except requests.Timeout as timed_out:
+        print("Connection timed out. Sorry.")
+        print(timed_out)
+    except Exception as e_unexpected:
+        print(f"crap, there was an error: {e_unexpected}")
+    assert  page != None # sick of that weird "'page' is not defined" error.
+    pagetext = read_response(page)
+    scrape(pagetext)
