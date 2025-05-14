b5c713686cdf777b230ea2bd20bded56ca814e2d
Dominic--Guye <sr8042mu@go.minnstate.edu>
2025-05-14 10:30:19 -0500

    corrected the name

--- test_scraper.py
+++ test_scraper.py
@@ -4,7 +4,7 @@ import requests
 import bs4
 
 class TestCorrectClasses(unittest.TestCase):
-    def get_page_returns_response_object(self): #make sure the output of get_page is a requests Response object
+    def test_get_page_returns_response_object(self): #make sure the output of get_page is a requests Response object
         response = scraper.get_page("https://www.example.com")
         self.assertIsInstance(response, requests.models.Response)
 
